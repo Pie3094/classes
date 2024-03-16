cc4ea32b7cc636844982be049a402030369b9769
Pie3094 <145662095+Pie3094@users.noreply.github.com>
2024-03-16 11:01:28 +0100

    Add files via upload

---
+++ classi-project/src/main/java/classe/classi/project/ClassiProjectApplication.java
@@ -0,0 +1,13 @@
+package classe.classi.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassiProjectApplication.class, args);
+	}
+
+}
---
+++ classi-project/src/test/java/classe/classi/project/ClassiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package classe.classi.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
